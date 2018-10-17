f4b6506034f28269760a8d3acc62591dc9e2b4f3
sternj <binaryman00010@gmail.com>
2018-10-17 09:50:26 -0400

    Did stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
     has_many :takes_class
     has_many :t_as_for
     has_many :teaches_class
-    has_many :registrations, through: :takes_class, class_name: 'Course'
-    has_many :taships, through: :t_as_class, class_name: 'Course'
-    has_many :professorships, through: :teaches_class, class_name: 'Course'
+    has_many :registrations, through: :takes_class, source: :courses
+    has_many :taships, through: :t_as_class, source: :courses
+    has_many :professorships, through: :teaches_class, source: :courses
     # has_many courses, through: :t_as_for
     # has_many courses, through: :teaches_class
 end
