54261a688011dd4ae56aef1275e5c897e65c4c49
sternj <binaryman00010@gmail.com>
2018-11-06 23:44:32 -0500

    Made the inline zip validator work

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -10,7 +10,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{SecureRandom.uuid}"
+    "uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
