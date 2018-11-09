944cd10cf954cb347e7caa72001ea3d82f6fd63c
tonywang1997 <36345394+tonywang1997@users.noreply.github.com>
2018-11-09 17:10:17 -0500

    user sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    byebug
     if @user.save
       log_in @user
       flash[:success] = "Welcome!"
