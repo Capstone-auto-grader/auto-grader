4c7de51c864b7fd27a773b1d1b986c30d57ceb03
Eli <elazare@brandeis.edu>
2018-11-09 16:22:11 -0500

    Added upload to S3 (hopefully)

--- app/jobs/validate_zip_file_job.rb
+++ app/jobs/validate_zip_file_job.rb
@@ -10,6 +10,13 @@ class ValidateZipFileJob < ApplicationJob
     uploader.retrieve_from_store! file_name
     if validate(uploader.path, structure_hash)
       # upload to S3
+      buckob = S3_BUCKET.objects[file_name]
+      buckob.write(
+        file: file_name
+        acl: :private_read
+      )
+      submission.zip_uri = buckob.public_url
+      submission.save
       puts "VALIDATED"
     else
       # Send out an email error message
