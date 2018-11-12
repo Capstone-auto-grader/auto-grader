f3a4cc26a01c0dfc096f74fd94cae2d4c7912b84
cmiddlemiss <cmiddlemiss@brandeis.edu>
2018-11-11 19:33:59 -0500

    all tests fixed other than ones that are actually failing

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -69,6 +69,6 @@ class AssignmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def assignment_params
-      params.require(:assignment).permit(:class_id, :assignment_test, :due_date)
+      params.require(:assignment).permit(:class_id, :name, :assignment_test, :due_date)
     end
 end
