ed4df1bb331ae2dfb13e2cf25d05c6f91d013c73
sternj <binaryman00010@gmail.com>
2018-11-12 22:17:18 -0500

    Assignment ui (#11)
    * session view
    
    * assignment quiklink

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,13 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/create', to: 'assignments#new' 
   root 'welcome#index'
   post 'grades', to: 'accept_grade#accept_grade'
 
