9608e61b649c77a32280348f48c71ef27526a788
sternj <binaryman00010@gmail.com>
2018-11-12 22:21:59 -0500

    added an extra end statement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,19 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
-def create
-  user = User.find_by(email: params[:session][:email].downcase)
-  if user && user.authenticate(params[:session][:password])
-    log_in user
-    unless session.has_key? :forwarding_url
-      redirect_to user
-      return
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      unless session.has_key? :forwarding_url
+        redirect_to user
+        return
+      end
+      redirect_to session[:forwarding_url]
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
-
-
-    redirect_to session[:forwarding_url]
-
-  else
-    flash.now[:danger] = 'Invalid email/password combination'
-    render 'new'
-
   end
 end
   def destroy
