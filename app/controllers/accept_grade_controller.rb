c6073eb798eb89bd4cbbe70125c4ebb5f26465b7
sternj <binaryman00010@gmail.com>
2018-11-12 20:25:55 -0500

    made some minor controller updates

--- app/controllers/accept_grade_controller.rb
+++ app/controllers/accept_grade_controller.rb
@@ -1,11 +1,12 @@
 class AcceptGradeController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
   def accept_grade
     status = params[:status]
     if status == "ok"
       submission = Submission.find(params[:id].to_i)
       if submission
-        submission.grade = (params[:number_of_tests].to_f - params[:number_of_failures].to_f) / params[:number_of_tests]
+        # TODO: Convert grade to int on frontend
+        submission.grade = ((params[:number_of_tests].to_f - params[:number_of_failures].to_f) / params[:number_of_tests]) * 100
         submission.save!
       end
     end
