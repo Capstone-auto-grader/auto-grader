9b2d0c8a477e7305d18de9e140bdda435c5e4428
sternj <binaryman00010@gmail.com>
2018-11-12 22:18:04 -0500

    Added some login_required

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,21 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+def create
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    log_in user
+    unless session.has_key? :forwarding_url
       redirect_to user
-    else
-      @log_in_failed = true
-      render 'new'
+      return
     end
+
+    redirect_to session[:forwarding_url]
+
+  else
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
+
   end
 
   def destroy
