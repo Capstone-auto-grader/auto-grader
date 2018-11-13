b867685b99a48eb76df5bf4a3313ace6bb20ed79
sternj <binaryman00010@gmail.com>
2018-11-13 11:27:37 -0500

    Merged branch for assignment UI (#14)
    * session view
    
    * assignment quiklink

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,15 @@
 class SessionsController < ApplicationController
+
   def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      unless session.has_key? :forwarding_url
-        redirect_to user
-        return
-      end
-      redirect_to session[:forwarding_url]
+      redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      @log_in_failed = true
+
       render 'new'
     end
   end
