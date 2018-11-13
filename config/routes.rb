d470a9e9f20f31e970d5cfda4bf5268550014fda
tonywang1997 <36345394+tonywang1997@users.noreply.github.com>
2018-11-12 22:11:26 -0500

    assignment quiklink

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/create', to: 'assignments#new' 
   root 'welcome#index'
   post 'grades', to: 'accept_grade#accept_grade'
 
