28cee64efb02e06eeef86fc785eb346a9a93e4a3
sternj <binaryman00010@gmail.com>
2018-11-29 20:31:00 -0500

    Added grades

--- db/migrate/20181129221204_create_grades.rb
+++ db/migrate/20181129221204_create_grades.rb
@@ -1,9 +1,7 @@
 class CreateGrades < ActiveRecord::Migration[5.2]
   def change
     create_table :grades do |t|
-      t.integer :student_id
-      t.integer :ta_id
-      t.integer :assignment_id
+      t.decimal :grade
 
       t.timestamps
     end
