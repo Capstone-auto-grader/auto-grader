2f4eff1140ca7da433e3ebd6018609c298b1ef55
sternj <binaryman00010@gmail.com>
2018-12-02 22:50:51 -0500

    added assignment algorithm

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,2 +1,18 @@
 module AssignmentsHelper
+  def assign(students, tas, conflicts)
+    shuffled_students = students.shuffle
+    ret_hash = tas.map { |t| [t,[]] }.to_h
+    while ! shuffled_students.empty?
+      tmp = []
+      while conflicts[tas[0]].include?(student = shuffled_students.pop)
+        tmp << student
+      end
+      if !student.nil?
+        ret_hash[tas[0]] << student
+      end
+      shuffled_students += tmp
+      tas = tas.rotate
+    end
+    ret_hash
+  end
 end
