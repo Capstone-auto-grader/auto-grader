c10c88a3c6af1591ec4b44a383181ae18898be5d
Eli Esrig <elazare@brandeis.edu>
2018-12-07 15:06:47 -0500

    Wrote password_reset create function & added relevant User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :reset_token
     has_many :takes_class
     has_many :t_as_class
     has_many :teaches_class
@@ -14,4 +15,15 @@ class User < ApplicationRecord
                        uniqueness: { case_sensitive: false }
     has_secure_password
 
+    #Sets password reset attributes
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    #Sends password reset email
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
 end
