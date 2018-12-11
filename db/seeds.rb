f8c3c1c8e4449388cca9fa6fef801ddbadb03d20
sternj <binaryman00010@gmail.com>
2018-12-10 21:14:46 -0500

    Changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,36 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+#
+# studentscourse1 = []
+# studentscourse2 = []
+# studentscourse3 = []
+# 10.times do
+#   studentscourse1 << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+# end
+# 10.times do
+#   studentscourse2 << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+# end
+# 10.times do
+#   studentscourse3 << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+# end
+# courses = []
+#
+# 3.times do
+#   courses << Course.create(name: Faker::TwinPeaks.location)
+# end
+#
+# courses[0].students << studentscourse1
+# courses[1].students << studentscourse2
+# courses[2].students << studentscourse3
+# courses[0].tas
+#
 
-studentscourse1 = []
-studentscourse2 = []
-studentscourse3 = []
-10.times do
-  studentscourse1 << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
-end
-10.times do
-  studentscourse2 << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
-end
-10.times do
-  studentscourse3 << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
-end
-courses = []
+ta = User.create(name: Faker::Name.name, email:'ta@autograder.com', password: '123456')
+professor = User.create(name: Faker::Name.name, email: 'prof@autograder.com', password: '123456')
+student = User.create(name: Faker::Name.name, email: 'student@autograder.com', password: '123456')
 
-3.times do
-  courses << Course.create(name: Faker::TwinPeaks.location)
-end
-
-courses[0].students << studentscourse1
-courses[1].students << studentscourse2
-courses[2].students << studentscourse3
-courses[0].tas
+course = Courses.create(name: Faker::TwinPeaks.location)
+course.tas << ta
+course.professors << professor
+course.students << student
