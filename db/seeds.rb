2fc28599ae43b9d4aa417a0e5197226203a472d1
sternj <binaryman00010@gmail.com>
2018-12-17 12:59:36 -0500

    View message (#29)
    * edit page
    
    * seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,9 +34,17 @@ require 'faker'
 
 ta = User.create(name: 'Zihao Wang', email:'ta@autograder.com', password: '123456')
 professor = User.create(name: 'Calum Middlemiss', email: 'prof@autograder.com', password: '123456')
+
 student = User.create(name: 'Eli Esrig', email: 'student@autograder.com', password: '123456')
 
-course = Course.create(name: Faker::TwinPeaks.location)
-course.tas << ta
+course = Course.create(name: 'COSI 166')
+course1 = Course.create(name: 'COSI 12B')
 course.professors << professor
-course.students << student
+course1.professors << professor
+10.times do
+  course.students << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+ end
+
+ 10.times do
+   course1.students << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+  end
