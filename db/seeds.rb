c9d0e26efc8c3c2a7ca26a5d166edc1f4eddd991
tonywang1997 <36345394+tonywang1997@users.noreply.github.com>
2018-12-17 10:19:39 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,14 +32,18 @@ require 'faker'
 # courses[0].tas
 #
 
-ta = User.create(name: Faker::Name.name, email:'ta@autograder.com', password: '123456')
-professor = User.create(name: Faker::Name.name, email: 'prof@autograder.com', password: '123456')
-student = User.create(name: Faker::Name.name, email: 'student@autograder.com', password: '123456')
+ta = User.create(name: 'Zihao Wang', email:'ta@autograder.com', password: '123456')
+professor = User.create(name: 'Sam Stern', email: 'prof@autograder.com', password: '123456')
+student = User.create(name: 'Eli Esrig', email: 'student@autograder.com', password: '123456')
 
-course = Course.create(name: Faker::TwinPeaks.location)
-course1 = Course.create(name: Faker::TwinPeaks.location)
-course.tas << ta
+course = Course.create(name: 'COSI 166')
+course1 = Course.create(name: 'COSI 12B')
 course.professors << professor
-course.students << student
 course1.professors << professor
-course1.students << ta
+10.times do
+  course.students << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+ end
+
+ 10.times do
+   course1.students << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+  end
