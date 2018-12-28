bf9b14f9d6904f21acd70a407b32c92d5ecf7e24
sternj <binaryman00010@gmail.com>
2018-12-28 17:20:28 -0500

    Changed assignment view and added labels to form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_055648) do
+ActiveRecord::Schema.define(version: 2018_12_27_145224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2018_12_27_055648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "attachment"
-    t.string "zip_uri"
+    t.text "zip_uri"
     t.bigint "assignment_id"
     t.integer "latte_id"
     t.integer "tests_passed"
