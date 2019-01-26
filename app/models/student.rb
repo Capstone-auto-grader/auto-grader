2f9fed2922bd9c554692af6bdda8468fba92379d
AriCarr <acarr@brandeis.edu>
2019-01-26 18:41:56 -0500

    Added UI for TAs to declare conflicts of interest (#47)

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,5 @@ class Student < ApplicationRecord
   has_many :assignments, through: :submissions
 
   has_many :ta_conflicts, foreign_key: :conflict_student_id
+  has_many :conflict_tas, through: :ta_conflicts
 end
