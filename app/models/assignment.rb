0a0288ffddce6976bfb9a8ae020c65efbba8cba5
Ari Carr <acarr@brandeis.edu>
2019-02-11 01:53:57 -0500

    Fixed assignment model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,7 +2,6 @@ class Assignment < ApplicationRecord
   belongs_to :course
   has_many :submissions
   has_many :students, through: :submissions
-  has_many :grades
 
   belongs_to :resubmit, class_name: 'Assignment', foreign_key: :resubmit_id, optional: true
 
