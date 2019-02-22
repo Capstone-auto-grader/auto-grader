250749b39595f9bdb975fff2ba36a5b74e0a9050
Ari Carr <acarr@brandeis.edu>
2019-02-22 18:42:15 -0500

    Allow invalid submissions to be resubmitted

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -22,14 +22,18 @@ class Submission < ApplicationRecord
 
   def final_grade
     return final_grade_override unless final_grade_override.nil?
-    return 0 if !is_valid || zip_uri.nil?
 
     test_weight = assignment.test_grade_weight / 100.0
     ta_weight = 1 - test_weight
 
     ta_grade = self.ta_grade || original.ta_grade
 
-    original_result = (test_weight * test_grade) + (ta_weight * ta_grade)
+    if !is_valid || zip_uri.nil?
+      test = 0
+    else
+      test = (test_weight * test_grade)
+    end
+    original_result = test + (ta_weight * ta_grade)
     if is_resubmission? || !resubmission.grade_received
       result = original_result
     else
