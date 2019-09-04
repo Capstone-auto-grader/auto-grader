013be717ea52796892fdf7d8c701cf54b95aa998
sternj <binaryman00010@gmail.com>
2019-09-03 22:40:41 -0400

    changed bucket

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -7,5 +7,5 @@ Aws.config.update({
                   })
 
 
-S3_BUCKET = Aws::S3::Resource.new.bucket('auto-grader')
+S3_BUCKET = Aws::S3::Resource.new.bucket('auto-grader-bucket')
 EC2_INSTANCE = Aws::EC2::Instance.new('i-08e1f3ee6c0bf2f97')
