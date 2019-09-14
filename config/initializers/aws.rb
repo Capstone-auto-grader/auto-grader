c28d5fd6564d74b41a4145d4f5913990d1797094
sternj <binaryman00010@gmail.com>
2019-09-14 12:52:38 -0400

    added stuff

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,15 @@
 require 'aws-sdk'
 
 Aws.config.update({
-    region: 'us-east-1',
+    region: 'us-east-2',
     access_key_id: ENV['ACCESS_KEY'],
     secret_access_key: ENV['SECRET_KEY'],
-                  })
+})
 
 
 S3_BUCKET = Aws::S3::Resource.new.bucket('auto-grader-bucket')
+Aws.config.update({
+      region: 'us-east-1'
+})
+
 EC2_INSTANCE = Aws::EC2::Instance.new('i-0cf13c8b86e3acce8')
