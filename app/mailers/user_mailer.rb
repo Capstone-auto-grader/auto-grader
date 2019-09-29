dfb2a7b89a19229e821bb3a9ee9001c4ed555509
sternj <binaryman00010@gmail.com>
2019-09-29 12:30:16 -0400

    AAAAAAAAAAAAAA

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,14 @@
 class UserMailer < ApplicationMailer
 
-    def password_reset(user)
+    def password_reset(user, request)
         @user = user
+        @request = request
         mail to: user.email, subject: "Password reset", from: 'noreply@autograder.me'
     end
 
-    def welcome_email(user)
+    def welcome_email(user, request)
         @user = user
+        @request = request
         mail to: user.email, subject: "Welcome to AutoGrader", from: 'noreply@autograder.me'
     end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'yourdomain.com',
+      :domain => 'production.q2bvedtkki.us-east-1.elasticbeanstalk.com',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
