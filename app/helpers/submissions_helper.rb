2ef82a377122d84908b0e10f7972ac0d7d613648
sternj <binaryman00010@gmail.com>
2019-10-08 22:25:14 -0400

    a

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -3,7 +3,7 @@ module SubmissionsHelper
     return 'No submission' if submission.zip_uri.nil?
     return 'Pending' unless submission.grade_received
 
-    return 'No tests' if submission.assignment.test_grade_weight.zero?
+
 
     submission.is_valid ? submission.test_grade : 'Invalid submission'
   end
